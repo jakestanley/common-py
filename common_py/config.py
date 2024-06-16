c78855198f3a3963c9827e13aab4edae02bc2e87
Jake Stanley <prudentia713@gmail.com>
2024-06-16 23:46:47 +0100

    GetConfigPath and LoadConfig require app_name argument

--- common_py/config.py
+++ common_py/config.py
@@ -2,15 +2,15 @@ import appdirs
 import os
 import json
 
-def GetConfigPath(app_name: str = None) -> str:
+def GetConfigPath(app_name: str) -> str:
 
     config_dir = appdirs.user_config_dir(appname=app_name, appauthor="com.github.jakestanley")
     os.makedirs(config_dir, exist_ok=True)
     return os.path.join(config_dir, "config.json")
 
-def LoadConfig() -> dict:
+def LoadConfig(app_name: str) -> dict:
 
-    config_path = GetConfigPath()
+    config_path = GetConfigPath(app_name)
     if os.path.exists(config_path):
         with open(config_path, "r") as f:
             return json.load(f)
