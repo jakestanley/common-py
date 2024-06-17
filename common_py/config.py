4ee0badd96e79b2d8d49c776e6e55340947e30d6
Jake Stanley <prudentia713@gmail.com>
2024-06-17 22:45:41 +0100

    make config an abstract class requiring implementations of _DefaultConfig and _PrepareSave

--- common_py/config.py
+++ common_py/config.py
@@ -2,25 +2,38 @@ import appdirs
 import os
 import json
 
-class Config:
-    def __init__(self, app_name: str, default_config: dict = {}) -> None:
+from abc import ABC, abstractmethod
+
+class Config(ABC):
+    def __init__(self, app_name: str) -> None:
         self.app_name = app_name
-        self.config = LoadConfig(app_name, default_config)
+        self.config = _LoadConfig(app_name, self._DefaultConfig())
+
+    @abstractmethod
+    def _DefaultConfig(self):
+        pass
+
+    @abstractmethod
+    def _PrepareSave(self):
+        pass
 
     def Save(self):
-        cfg_path = GetConfigPath(self.app_name)
+        # instruct the child class to prepare self.config for saving
+        self._PrepareSave()
+        # Save config to file
+        cfg_path = _GetConfigPath(self.app_name)
         with open(cfg_path, "w") as f:
             json.dump(self.config, f, indent=4)
 
-def GetConfigPath(app_name: str) -> str:
+def _GetConfigPath(app_name: str) -> str:
 
     config_dir = appdirs.user_config_dir(appname=app_name, appauthor="com.github.jakestanley")
     os.makedirs(config_dir, exist_ok=True)
     return os.path.join(config_dir, "config.json")
 
-def LoadConfig(app_name: str, default_config={}) -> dict:
+def _LoadConfig(app_name: str, default_config={}) -> dict:
 
-    config_path = GetConfigPath(app_name)
+    config_path = _GetConfigPath(app_name)
     if os.path.exists(config_path):
         with open(config_path, "r") as f:
             return json.load(f)
